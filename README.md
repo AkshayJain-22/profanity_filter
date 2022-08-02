d7ef54ce8cfe0254f1357c2083dbfad327e10b8f
akshay <akshay22.11.1995@gmail.com>
2022-08-02 14:50:20 +0530

    index page added

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Repeat the above process with different patterns to identify the loopholes.
 
 # Enjoy
 
+HEROKU_EMAIL: akshay22.08.1995@gmail.com
+API_KEY: 715209f5-2cba-4292-b901-2ccbddcd4678
+HEROKU_APP_NAME: profanity-filter
--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request, render_template, Response
+from flask_cors import CORS, cross_origin
 
+import os
 app=Flask(__name__)
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Hello there API under progress"
+    return render_template('index.html')
 
 if __name__=="__main__":
     app.run(debug=True)
