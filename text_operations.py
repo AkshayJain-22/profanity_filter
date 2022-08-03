dda81e97f25baa73fe974271e7deada1382145f4
akshay <akshay22.11.1995@gmail.com>
2022-08-03 21:34:29 +0530

    stopwords

--- text_operations.py
+++ text_operations.py
@@ -1,8 +1,9 @@
 import nltk
 stemmer = nltk.SnowballStemmer("english")
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
 import string
-stopword=set(stopwords.words('english'))
+#stopword=set(stopwords.words('english'))
+stopword=["i", "br", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 import re 
 
 def clean_text(text)->str: # this function will clean the text and return it.
