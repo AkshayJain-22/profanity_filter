f9b59317ba8acafb9aae5a80c585dd97cf39ed3b
akshay <akshay22.11.1995@gmail.com>
2022-08-19 22:32:51 +0530

    'logging_initiated_in_module'

--- ml_models.py
+++ ml_models.py
@@ -1,6 +1,7 @@
 import pickle
 from text_operations import clean_text
 import logging
+from flask import Flask
 
 def abuse_detector(comment)->int:   
     with open("models/vectorization_model_banned_wrds.sav",'rb') as file:
@@ -9,7 +10,7 @@ def abuse_detector(comment)->int:
         model = pickle.load(file)  #load the classification model we saved earlier
     #with open("clean_text_func.sav",'rb') as file:
         #clean_text = pickle.load(file)  #load the text cleaning fucntion
-    module = __name__
+    module = Flask(__name__)
     gunicorn_logger = logging.getLogger('gunicorn.error')
     module.logger.handlers = gunicorn_logger.handlers
     module.logger.setLevel(gunicorn_logger.level)
