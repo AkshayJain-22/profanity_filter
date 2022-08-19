22b930b4076211a9bd1b34c06762e5629bbdf49a
akshay <akshay22.11.1995@gmail.com>
2022-08-19 22:08:13 +0530

    'logging_initiated'

--- app.py
+++ app.py
@@ -7,12 +7,17 @@ from accuracy_finder import accuracy_record
 from db_operations import populate_table
 import pandas as pd
 import os
+import logging
 
 comment=''
 prediction_output=''
 updated_comment=''
 table_name = 'final_records'
 app=Flask(__name__)
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
@@ -37,6 +42,7 @@ def begin():
 
     if len(comment) == 0:
         return render_template('index.html')
+    app.logger.info("Entering the prediction")
 
     prediction = abuse_detector(comment)         #calling our detector function (ML model)
     filtered_comment = profanity_filter(comment) #profanity filter model returns *ed comment
@@ -45,24 +51,31 @@ def begin():
         if ('**') in filtered_comment:           #check the prediction of profanity filter
             prediction_output='Abusive' 
             updated_comment = filtered_comment
-        else:                                    #both said not abusive so go for second step verification
-            second_prediction, filtered_comment = unique_letters_profanity(comment) #prediction returned by unique letters method
-            if second_prediction==0:             #if ML model again says not abusive
-                filtered_comment = profanity_filter(comment)
-                if ('*') in filtered_comment:    #if profanity filter says abusive
-                    prediction_output='Abusive'
-                    updated_comment= filtered_comment
-                else:                            #if both models says not abusive
-                    prediction_output='Not Abusive'
-                    updated_comment=comment
-            else:                                #if ML model says abusive on the second attempt
-                prediction_output='Abusive' 
-                updated_comment=filtered_comment
-    elif prediction==1:                         #if ML model says abusive in the first attempt
+            prediction=1                         #change the prediction to 1 i.e. Abusive
+    
+    #Go for second step verification irrespective of first output
+    second_prediction, filtered_comment = unique_letters_profanity(filtered_comment) #prediction returned by unique letters method
+    
+    if second_prediction==0:             #if ML model says not abusive
+        filtered_comment = profanity_filter(filtered_comment)
+        if ('**') in filtered_comment:    #if profanity filter says abusive
+            prediction_output='Abusive'
+            updated_comment= filtered_comment
+            prediction=1
+        else:                            #if both models says not abusive
+            prediction_output='Not Abusive'
+            updated_comment=comment
+    else:                                #if ML model says abusive on the second attempt
+        prediction_output='Abusive' 
+        updated_comment=filtered_comment
+        prediction=1
+
+    if prediction==1:                         #if ML model says abusive in the second attempt
         if ('**') not in filtered_comment:
             filtered_comment = profanity_filter_inner(comment)                                        
         prediction_output='Abusive'
         updated_comment=filtered_comment
+    app.logger.info("Returning the prediction")
 
     result={'prediction_output':prediction_output,'updated_comment':updated_comment}
     return render_template('index.html', result=result) # showing the result to the user
