046f84a510adea346dadab6b53a3d1980c9cedcc
akshay <akshay22.11.1995@gmail.com>
2022-09-15 16:37:51 +0530

    unicode_escape

--- auto_correct.py
+++ auto_correct.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def autocorrect(trimmed_sentence):
-    trimmed_dictionary = pd.read_csv('files/banned_words_final.csv')
+    trimmed_dictionary = pd.read_csv('files/banned_words_final.csv',encoding='unicode_escape')
     
     for index in range(len(trimmed_dictionary['trimmed'])):
         word = trimmed_dictionary['trimmed'][index]
