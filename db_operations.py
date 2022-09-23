bcf8880c140f11786581042cf2d825cedbedd36f
akshay <akshay22.11.1995@gmail.com>
2022-09-23 14:16:04 +0530

    Project_Secrets

--- db_operations.py
+++ db_operations.py
@@ -1,5 +1,11 @@
 import psycopg2
 import pandas as pd
+import os
+
+host = os.environ['host']
+user = os.environ['user']
+password = os.environ['password']
+db_name = os.environ['db_name']
 
 def run_syntax(db_connection: psycopg2, syntax: str) -> None:
     """
@@ -18,10 +24,10 @@ def populate_table(table_name: str, data: pd.DataFrame) -> None:
     :param df: The dataframe that we use for puplating the table.
     """
     db_connection = psycopg2.connect(
-        host='ec2-34-227-120-79.compute-1.amazonaws.com',
-        user='niwisgpkanhoxg',
-        password='db78f028a6f0ef2daba54675f2aa2da4991abc7d1d56d9fde96d78927602002c',
-        dbname='ddieop547ho0p4',
+        host=host,
+        user=user,
+        password=password,
+        dbname=db_name,
     )
     
     # Inject data
@@ -33,10 +39,10 @@ def populate_table(table_name: str, data: pd.DataFrame) -> None:
 
 def read_data(table_name: str):
     db_connection = psycopg2.connect(
-    host='ec2-34-227-120-79.compute-1.amazonaws.com',
-    user='niwisgpkanhoxg',
-    password='db78f028a6f0ef2daba54675f2aa2da4991abc7d1d56d9fde96d78927602002c',
-    dbname='ddieop547ho0p4',
+        host=host,
+        user=user,
+        password=password,
+        dbname=db_name,
     )
     query = pd.read_sql_query(f"SELECT * FROM {table_name}",db_connection)
     df = pd.DataFrame(query,columns=['comment','updated_comment','prediction','score'])
